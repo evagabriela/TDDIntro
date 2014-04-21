852f6573bb97396e96f55453960a736c816e72eb
Eva Zamudio <gabriela.osu@gmail.com>
2014-04-20 23:16:09 -0700

    refactor code for the first test

--- src/main/java/com/thoughtworks/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/accountbalance/Account.java
@@ -4,10 +4,11 @@ package com.thoughtworks.accountbalance;
  * Created by evazamudio on 2014-04-20.
  */
 public class Account {
-    public Integer balance;
+    public Integer balance = 0;
+
 
     public void deposit(int i) {
-        
+        balance = balance + i;
     }
     
 }
