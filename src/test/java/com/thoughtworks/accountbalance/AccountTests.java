9f769d1098e5fd44317f2ad82201f6b20fb8b3a0
Eva Zamudio <gabriela.osu@gmail.com>
2014-04-20 23:31:20 -0700

    update refactored code for the final tests in the account exercise

--- src/test/java/com/thoughtworks/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/accountbalance/AccountTests.java
@@ -6,16 +6,16 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class AccountTests {
+    private final Account account = new Account();
+
     @Test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-        Account account = new Account();
         account.deposit(50);
         assertThat(account.balance, is(50));
     }
 
     @Test
     public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
-        Account account = new Account();
         account.balance = 50;
         account.withdraw(10);
         assertThat(account.balance, is(40));
@@ -23,7 +23,6 @@ public class AccountTests {
 
     @Test
     public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
-        Account account = new Account();
         account.withdraw(20);
         assertThat(account.balance, is(0));
     }
