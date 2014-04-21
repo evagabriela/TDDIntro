49f3b407f0801548877d0a8026462c044111a78d
Eva Zamudio <gabriela.osu@gmail.com>
2014-04-20 22:43:53 -0700

    add factorial refactored code

--- src/main/java/com/thoughtworks/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/factorial/Factorial.java
@@ -2,15 +2,18 @@ package com.thoughtworks.factorial;
 
 public class Factorial {
     public Integer compute(int i) throws IllegalArgumentException{
-        if (i == 1 || i == 0){
-            return 1;
-        } if (i == 2){
-            return 2;
-        }if (i == 3){
-            return 6;
-        } if (i < 0) {
+        if (i < 0) {
             throw new IllegalArgumentException();
+        } if (i == 1 || i == 0){
+            return 1;
         }
-        return null;
+        int factorial_var = 1;
+        for ( int num = 1; num <= i; num++){
+            factorial_var =num * factorial_var;
+        }
+        return factorial_var;
     }
 }
+
+// 3 => 3X2X1
+// 5 => 5x4x3x2x1
