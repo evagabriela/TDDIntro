29df26c6a827fad80942abab8d853a0905cd9b02
Eva Zamudio <gabriela.osu@gmail.com>
2014-04-20 22:29:05 -0700

    add factorial should Throw Illegal Argument Exception when number is negative solution

--- src/main/java/com/thoughtworks/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/factorial/Factorial.java
@@ -1,13 +1,15 @@
 package com.thoughtworks.factorial;
 
 public class Factorial {
-    public Integer compute(int i) {
+    public Integer compute(int i) throws IllegalArgumentException{
         if (i == 1 || i == 0){
             return 1;
         } if (i == 2){
             return 2;
         }if (i == 3){
             return 6;
+        } if (i < 0) {
+            throw new IllegalArgumentException();
         }
         return null;
     }
