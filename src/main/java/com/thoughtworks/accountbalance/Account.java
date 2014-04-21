c8e04ac6d0ce62f98893ee8482fc52b7c460323d
Eva Zamudio <gabriela.osu@gmail.com>
2014-04-20 23:04:03 -0700

    update account balance exercise - first test fail

---
+++ src/main/java/com/thoughtworks/accountbalance/Account.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.accountbalance;
+
+/**
+ * Created by evazamudio on 2014-04-20.
+ */
+public class Account {
+    public Integer balance;
+
+    public void deposit(int i) {
+        
+    }
+    
+}
--- src/test/java/com/thoughtworks/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/accountbalance/AccountTests.java
@@ -2,11 +2,16 @@ package com.thoughtworks.accountbalance;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class AccountTests {
-//    @Test
-//    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-//
-//    }
+    @Test
+    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
+        Account account = new Account();
+        account.deposit(50);
+        assertThat(account.balance, is(50));
+    }
 //
 //    @Test
 //    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
